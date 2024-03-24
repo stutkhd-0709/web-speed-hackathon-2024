ece574f816b54ab72862572a0d569cfbf4b3f1d5
Takahide Sato <sato_t@cyberowl.co.jp>
2024-03-24 12:41:39 +0900

    update max-age

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -1,6 +1,6 @@
 import { createMiddleware } from 'hono/factory';
 
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
-  c.res.headers.append('Cache-Control', 'max-age=86400');
+  c.res.headers.append('Cache-Control', 'max-age=1800');
   await next();
 });
